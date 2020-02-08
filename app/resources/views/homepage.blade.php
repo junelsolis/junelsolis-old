a963a6a572ec3f382f011edf6ade89837d684f04
Junel Solis <junel.solis@gmail.com>
2020-02-09 01:03:15 +0500

    clean up the blade template and css

--- app/resources/views/homepage.blade.php
+++ app/resources/views/homepage.blade.php
@@ -7,31 +7,10 @@
     <body class='bg-grey-600'>
         <div id='app' class='h-screen bg-grey-100 flex items-center justify-end'>
             <div class='panel'>
-                <div class='left-nav'>
-                    <div class='flex-grow p-12 flex flex-col items-center justify-between relative'>
-                        <div class='top-logo'>
-                            <img src='/images/logo2.svg' class='h-12 w-auto'>
-                        </div>
-                        <div class='bottom'>
-                            <div class='flex items-center absolute left-0 right-0'>
-                                <img src='/images/logo.svg' class='h-4 w-auto'>
-                                <p class='flex text-grey-400'>&nbsp;/&nbsp;20<span class='text-grey-600 font-bold'>{{ date('y') }}</span></p>
-                            </div>
-                            
-                        </div>
-                    </div>
-                    <div class='flex flex-col'>
-                        <div class='flex-grow border-r border-grey-300 w-1'></div>
-                        <div class='flex flex-col'>
-                            <a href='' class='font-bold text-grey-300 py-1'>Home</a>
-                            <a href='' class='font-bold text-grey-300 py-1'>Projects</a>
-                            <a href='' class='font-bold text-grey-300 py-1'>Tech Stack</a>
-                            <a href='' class='font-bold text-grey-300 py-1'>Contact</a>
-                        </div>
-                        <div class='flex-grow'></div>
-                    </div>
-                </div>
+                @include('left-nav')
 
+
+                
             </div>
         </div>
         
---
+++ app/resources/views/left-nav.blade.php
@@ -0,0 +1,24 @@
+<div class='left-nav'>
+    <div class='flex-grow p-12 flex flex-col items-center justify-between relative'>
+        <div class='top-logo'>
+            <img src='/images/logo2.svg'>
+        </div>
+        <div class='bottom'>
+            <div>
+                <img src='/images/logo.svg'>
+                <p>&nbsp;/&nbsp;20<span>{{ date('y') }}</span></p>
+            </div>
+            
+        </div>
+    </div>
+    <div class='flex flex-col'>
+        <div class='flex-grow border-r border-grey-300 w-1'></div>
+        <div class='flex flex-col'>
+            <a href='' class='font-bold text-grey-300 py-1'>Home</a>
+            <a href='' class='font-bold text-grey-300 py-1'>Projects</a>
+            <a href='' class='font-bold text-grey-300 py-1'>Tech Stack</a>
+            <a href='' class='font-bold text-grey-300 py-1'>Contact</a>
+        </div>
+        <div class='flex-grow'></div>
+    </div>
+</div>
