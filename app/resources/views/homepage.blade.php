2f2efd387ee9a3c917f307cf63140c91be2c5790
Junel Solis <junel.solis@gmail.com>
2020-02-09 05:30:38 +0500

    adjust width

--- app/resources/views/homepage.blade.php
+++ app/resources/views/homepage.blade.php
@@ -21,7 +21,7 @@
                             <img src='/images/hero-image.svg' class='rellax' data-rellax-speed='-6' data-rellax-zindex='-100'>
                             <div class='w-3/5'>
                                 <h1 class='rellax uppercase leading-tight' style='font-size:5rem;' data-rellax-speed='-2'>Web<br>Applications</h1>
-                                <h3 class='rellax mt-6 normal-case text-grey-400' data-rellax-speed='-3'>Elegant, fully-tested and customized to meet your requirements</h3>
+                                <h3 class='rellax mt-6 normal-case text-grey-400 w-3/5' data-rellax-speed='-3'>Elegant, fully-tested and customized to meet your requirements</h3>
                             </div>
 
                             
