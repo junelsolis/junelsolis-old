716d0bc7d528e04fde5792c188ee12275f4d8738
Junel Solis <junel.solis@gmail.com>
2020-02-09 05:46:16 +0500

    adjust parallax

--- app/resources/views/homepage.blade.php
+++ app/resources/views/homepage.blade.php
@@ -19,9 +19,9 @@
                     <main class='content h-full w-full'>
                         <div id='landing' class='w-full h-full pt-48'>
                             <img src='/images/hero-image.svg' class='rellax' data-rellax-speed='-6' data-rellax-zindex='-100'>
-                            <div class='w-3/5'>
-                                <h1 class='rellax uppercase leading-tight' style='font-size:5rem;' data-rellax-speed='-2'>Web<br>Applications</h1>
-                                <h3 class='rellax mt-6 normal-case text-grey-400 w-3/5' data-rellax-speed='-3'>Elegant, fully-tested and customized to meet your requirements</h3>
+                            <div class='w-3/5 rellax' data-rellax-speed='-1'>
+                                <h1 class='uppercase leading-tight' style='font-size:5rem;'>Web<br>Applications</h1>
+                                <h3 class='mt-6 normal-case text-grey-400 w-3/5'>Elegant, fully-tested and customized to meet your requirements</h3>
                             </div>
 
                             
