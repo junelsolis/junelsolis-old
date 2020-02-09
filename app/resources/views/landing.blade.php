bee1040bbc2146953220ef23440fc294bd62e746
Junel Solis <junel.solis@gmail.com>
2020-02-09 08:34:16 +0500

    add a link row

--- app/resources/views/landing.blade.php
+++ app/resources/views/landing.blade.php
@@ -3,10 +3,16 @@
     <div class='w-3/5 rellax' data-rellax-speed='-1'>
         <h1 class='uppercase leading-tight' style='font-size:5rem;'>Web<br>Applications</h1>
         <h3 class='mt-6 normal-case text-grey-400 w-3/5'>Elegant, fully-tested and customized to meet your requirements</h3>
-        <div class='flex items-center mt-6 rellax' data-rellax-speed='1'>
-            <a href='https://www.github.com/junelsolis' target='_blank' class='text-grey-500 text-xl'><i class="fab fa-github"></i></a>
-            <a href='mailto:info@junelsolis.com' target='_blank' class='ml-3 text-grey-500 text-xl'><i class="fas fa-at"></i></a>
-            <a href='https://wa.me/254790503144' target='_blank' class='ml-3 text-grey-500 text-xl'><i class="fab fa-whatsapp"></i></a>
+        <div class='link-row rellax' data-rellax-speed='1'>
+            <div class='item'>
+                <a href='https://www.github.com/junelsolis' target='_blank'><i class="fab fa-github"></i></a>
+            </div>
+            <div class='item'>
+                <a href='mailto:info@junelsolis.com' target='_blank'><i class="fas fa-at"></i></a>
+            </div>
+            <div class='item'>
+                <a href='https://wa.me/254790503144' target='_blank'><i class="fab fa-whatsapp"></i></a>
+            </div>
         </div>
     </div>
 
