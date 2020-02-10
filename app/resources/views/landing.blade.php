9a956a6cac64649dd593c5e50a948737b6246394
Junel Solis <junel.solis@gmail.com>
2020-02-10 17:24:52 +0500

    remove parallax on link row

--- app/resources/views/landing.blade.php
+++ app/resources/views/landing.blade.php
@@ -3,7 +3,7 @@
     <div class='w-3/5 rellax' data-rellax-speed='-1'>
         <h1 class='uppercase leading-tight' style='font-size:5rem;'>Web<br>Applications</h1>
         <h3 class='mt-6 normal-case text-grey-400 w-3/5'>Elegant, fully-tested and customized to meet your requirements</h3>
-        <div class='link-row rellax' data-rellax-speed='1'>
+        <div class='link-row'>
             <a href='https://www.github.com/junelsolis' target='_blank' class='item'><i class="fab fa-github"></i></a>
             <a href='mailto:info@junelsolis.com' target='_blank' class='item'><i class="fas fa-at"></i></a>
             <a href='https://wa.me/254790503144' target='_blank' class='item'><i class="fab fa-whatsapp"></i></a>
