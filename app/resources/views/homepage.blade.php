55d7a97e95d5a785f10c35ebf6271085da379fdf
Junel Solis <junel@junelsolis.com>
2020-05-21 01:06:54 +0000

    add a stack

--- app/resources/views/homepage.blade.php
+++ app/resources/views/homepage.blade.php
@@ -6,19 +6,55 @@
         <title>Junel Solis | Full Stack Web Developer</title>
     </head>
     <body>
-        this is the homepage
+        <div id='app' class='homepage'>
+            <section class='landing'>
+                <div class='left'>left</div>
+                <div class='middle'>
+                    <div class='logo'>
+                        <div>
+                            <img src='/images/logo.svg' alt='Junel Solis Full Stack Web Developer Logo'>
+                        </div>
+                        <div class='ml-4'>
+                            <h1>Junel Solis</h1>
+                            <h2>Full Stack Web Developer</h2>
+                        </div>
+                    </div>
+                    <div class='stack'>
+                        <ul>
+                            <li>Laravel</li>
+                            <li>Vue</li>
+                            <li>Tailwind CSS</li>
+                            <li>MariaDB</li>
+                            <li>Docker</li>
+                            <li>Nginx</li>
+                            <li>Redis</li>
+                            <li>Linux</li>
+                        </ul>
+                        <ul class='mt-4'>
+                            <li>Stripe</li>
+                            <li>Paypal</li>
+                            <li>Mapbox</li>
+                            <li>Google Maps</li>
+                            <li>Wordpress API</li>
+                        </ul>
+                    </div>
+                    <div></div>
+                </div>
+                <div class='right'>right</div>
+            </section>
+        </div>
         
 
         <script src='/js/manifest.js'></script>
         <script src='/js/vendor.js'></script>
         <script src='/js/site.js'></script>
 
-        <script type="text/javascript" src='/js/rellax.min.js'></script>
+        <!-- <script type="text/javascript" src='/js/rellax.min.js'></script>
         <script>
             var rellax = new Rellax('.rellax', {
                 // center: true,
                 wrapper: '.scroller'
             });
-        </script>
+        </script> -->
     </body>
 </html>
--- app/webpack.mix.js
+++ app/webpack.mix.js
@@ -10,10 +10,6 @@ mix
     ])
 
     .copy('node_modules/rellax/rellax.min.js', 'public/js')
-    // .copy('node_modules/@fortawesome/fontawesome-free/js/all.min.js', 'public/js')
-    // .copyDirectory('node_modules/@fortawesome/fontawesome-free/svgs', 'public/svgs')
-
-    // .js('resources/js/homepage.js', 'public/js')
     .js('resources/js/site.js', 'public/js')
     
     .sass('resources/sass/site.scss', 'public/css')
