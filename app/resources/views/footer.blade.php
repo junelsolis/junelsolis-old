d7d64ea4795e4c41ae0b0e520cc4334091fb604f
Junel Solis <junel@junelsolis.com>
2020-05-24 02:38:03 +0000

    additional footer styles

--- app/resources/views/footer.blade.php
+++ app/resources/views/footer.blade.php
@@ -1,12 +1,18 @@
 <footer>
     <div class="bg-teal-700 px-6 py-12">
-        <img src='/images/logo-full-inverse.svg' alt='Junel Solis full stack web developer' class='w-64 h-auto mx-auto'>
+        <div class="container mx-auto">
+            <img src='/images/logo-full-inverse.svg' alt='Junel Solis full stack web developer' class='w-64 h-auto mx-auto'>
+        </div>
+
     </div>
-    <div class="bg-teal-800 px-6 py-3 text-center">
-        <span class="text-center text-teal-500 text-xs">Created using Laravel | Vue | Tailwind | Docker</span><br>
-        <span class="text-center text-xs text-teal-700">
-            <a href="https://github.com/junelsolis/junelsolis/commit/{{ Cache::get('git_commit') }}" target='_blank'>
-            {{ Cache::get('git_commit') }}</a>
-        </span>
+    <div class="bottom">
+        <div class="container mx-auto">
+            <span class="text-center text-teal-500 text-xs">Created using <a href='https://laravel.com'>Laravel</a> | <a href='https://vuejs.org'>Vue</a> | <a href='https://tailwindcss.com'>Tailwind</a> | <a href='https://docker.com'>Docker</a></span><br>
+            <span class="text-center text-xs text-teal-700">
+                <a href="https://github.com/junelsolis/junelsolis/commit/{{ Cache::get('git_commit') }}" target='_blank'>
+                    {{ Cache::get('git_commit') }}</a>
+            </span>
+        </div>
+
     </div>
 </footer>
