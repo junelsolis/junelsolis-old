e91cd4555ea5d45132c3f4403b9c79cb8515fd12
Junel Solis <junel@junelsolis.com>
2020-05-30 20:04:42 +0000

    eager load images

--- app/app/ProjectDemo.php
+++ app/app/ProjectDemo.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class ProjectDemo extends Model
 {
     protected $guarded = [];
+    protected $with = ['images'];
+
+
     public function images()
     {
         return $this->hasMany('App\ProjectDemoImage');
